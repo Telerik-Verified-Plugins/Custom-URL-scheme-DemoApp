5b59cf1775d9c1eb4ea22f1f26b243aba27e5442
Eddy Verbruggen <eddyverbruggen@gmail.com>
2014-12-03 20:04:18 +0000

    Demo app for the Custom URL scheme plugin

---
+++ CustomURL/scripts/demo.js
@@ -0,0 +1,24 @@
+// This function needs to be a first class citizen, so place it at the root of a .js file, or bind it to the window object.
+// Also, if you need to access Cordova functions inside this function, make sure 'deviceready' has fired
+function handleOpenURL(url) {
+    // wrapping in a little timeout, so it doesn't interfere with other app setup stuff
+    setTimeout(function() {
+        alert("App launch URL:\n" + url);
+    }, 300);
+};
+
+(function (global) {
+    var DemoViewModel,
+        app = global.app = global.app || {};
+
+    DemoViewModel = kendo.data.ObservableObject.extend({
+        composeEmail : function() {
+            // Change the 'mycoolapp' bit here if you're using a different URL scheme
+            window.location = "mailto:?subject=Custom URL scheme demo&body=mycoolapp://my/path?foo=bar"
+        }
+    });
+
+    app.demoService = {
+        viewModel: new DemoViewModel()
+    };
+})(window);
